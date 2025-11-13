fb9e7a4d867286a5ca45f31cb01abb8942334cb4
FORSYTH OKOEGUALE <forsythokoeguale01@gmail.com>
2025-11-13 17:16:41 +0100

    new commit

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -6,7 +6,7 @@ import SocialIcons from "./SocialIcons";
 
 export default function Hero() {
   return (
-    <section className="relative min-h-screen flex items-center justify-center overflow-hidden">
+    <section className="relative h-screen md:min-h-screen flex items-center justify-center overflow-hidden">
       {/* Mobile Background */}
       <div
         className="block md:hidden absolute inset-0 bg-cover bg-center"
@@ -39,63 +39,62 @@ export default function Hero() {
       ></div>
 
       {/* Centered Content */}
-      <div className="relative z-10 w-full px-6 py-12 h-full">
-        <div className="max-w-7xl mx-auto h-full">
-          <div className="flex flex-col lg:flex-row justify-center items-center gap-8 lg:gap-12 h-full">
+      <div className="relative z-10 w-full h-full flex flex-col md:px-6 md:py-12">
+        <div className="max-w-7xl mx-auto h-full w-full">
+          <div className="flex flex-col lg:flex-row justify-between items-center lg:gap-12 h-full">
             {/* Left Content */}
             <motion.div
-              className="relative z-20 text-center lg:text-left lg:left-10 pt-4 lg:pt-0"
+              className="relative z-20 text-center lg:text-left lg:left-10 pt-12 px-4 md:pt-4 lg:pt-0 flex-shrink-0"
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6 }}
             >
-                {/* Logo */}
-                <div className="flex justify-center lg:justify-start">
+              {/* Logo */}
+              <div className="flex justify-center lg:justify-start">
+                <Image
+                  src="/assets/logo.png"
+                  alt="Logo"
+                  width={900}
+                  height={300}
+                  quality={90}
+                  className="h-auto w-[240px] sm:w-[280px] md:w-[450px] lg:w-[500px] xl:w-[650px]"
+                />
+              </div>
+
+              {/* Tagline */}
+              <p className="lucky text-lg sm:text-xl md:text-3xl lg:text-3xl xl:text-[35px] rotate-[-2deg] mt-2 md:mt-0">
+                Freedom has a currency.
+              </p>
+
+              {/* Buy Button - Desktop Only */}
+              <div className="mt-8 hidden lg:block">
+                <motion.a
+                  href="#howtobuy"
+                  whileHover={{ scale: 1.06 }}
+                  whileTap={{ scale: 0.95 }}
+                >
                   <Image
-                    src="/assets/logo.png"
-                    alt="Logo"
-                    width={900}
-                    height={300}
-                    quality={90}
-                    className="h-auto w-[353px] sm:w-[250px] md:w-[450px] lg:w-[500px] xl:w-[650px]"
+                    src="/buynomad.png"
+                    alt="Buy Nomad"
+                    width={200}
+                    height={80}
+                    className="m-auto h-20"
                   />
-                </div>
-              <div className="relative bottom-10 ">
-                <div className="">
-                  {/* Tagline */}
-                  <p className="lucky pt-2 lg:pt-0 text-lg text-center sm:text-2xl md:text-3xl lg:text-3xl xl:text-[35px] rotate-[-2deg]">
-                    Freedom has a currency.
-                  </p>
-                </div>
+                </motion.a>
+              </div>
 
-                {/* Buy Button */}
-                <div className="mt-8 hidden lg:block">
-                  <motion.a
-                    href="#howtobuy"
-                    whileHover={{ scale: 1.06 }}
-                    whileTap={{ scale: 0.95 }}
-                    className=""
-                  >
-                    <Image
-                      src="/buynomad.png"
-                      alt="Buy Nomad"
-                      width={200}
-                      height={80}
-                      className="m-auto h-20 "
-                    />
-                  </motion.a>
-                </div>
-
-                {/* Social Icons */}
-                <div className="mt-6 hidden lg:block">
-                  <SocialIcons />
-                </div>
+              {/* Social Icons - Desktop Only */}
+              <div className="mt-6 hidden lg:block">
+                <SocialIcons />
               </div>
             </motion.div>
+
             {/* Right Image */}
             <motion.div
-              className="relative w-[500px] h-[500px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px] 
-              lg:w-[500px] lg:h-[500px] xl:w-[600px] xl:h-[600px] mt-auto lg:mt-0"
+              className="absolute bottom-0 left-1/2 -translate-x-1/2 w-[520px] h-[720px] z-10
+                sm:w-[380px] sm:h-[380px]
+                md:relative md:left-0 md:translate-x-0 md:w-[400px] md:h-[400px] 
+                lg:w-[500px] lg:h-[500px] xl:w-[600px] xl:h-[600px]"
               initial={{ opacity: 0, y: 20 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.6, delay: 0.2 }}
@@ -104,7 +103,7 @@ export default function Hero() {
                 src="/assets/hero.png"
                 alt="Nomad Pepe"
                 fill
-                className="object-contain"
+                className="object-contain object-bottom md:object-center"
                 priority
                 quality={90}
               />
